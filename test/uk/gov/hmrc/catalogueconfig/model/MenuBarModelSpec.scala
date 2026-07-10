c10c85a3a4af005800593065e2438b30be41e13a
James Patey <281954486+jamespatey-cap@users.noreply.github.com>
2026-07-10 08:27:37 +0100

    BDOG-3879: add serialization test for BannerMenu

--- test/uk/gov/hmrc/catalogueconfig/model/MenuBarModelSpec.scala
+++ test/uk/gov/hmrc/catalogueconfig/model/MenuBarModelSpec.scala
@@ -19,13 +19,15 @@ package uk.gov.hmrc.catalogueconfig.model
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import play.api.libs.json.Json
+import uk.gov.hmrc.catalogueconfig.menu.NavMenuService
+import uk.gov.hmrc.catalogueconfig.menu.NavMenuService.{createServiceUser, createUser, offboardUsers}
 
 class MenuBarModelSpec extends
   AnyWordSpec
   with Matchers:
 
   "MenuBarModel JSON representation" can{
-    "deserialize representation of menu DropdownSeparator" in {
+    "deserialize representation of menu elements" in {
       val json = Json.parse(
         """{
           |  "brand": {
@@ -73,5 +75,136 @@ class MenuBarModelSpec extends
         Page("Blog", "docs-blog", "/docs/blog")
         )
     }
-  }
 
+    "serialize representation of menu elements" in {
+      val menu = BannerMenu(
+        brand = TopMenu("MDTP", "brand", "/"),
+        topLevelLinks = Seq(
+          TopMenu("Users", "users", "/users"),
+          TopMenu("Teams", "teams", "/teams"),
+          TopMenu("Repositories", "repositories", "/repositories"),
+          TopMenu("Deployments", "deployments", None)
+        ),
+        dropdowns = Seq(
+          MenuDropdown(
+            id = "docs",
+            name = "Docs",
+            href = None,
+            items = Seq(
+              Page("Docs Home", "docs-home", "/docs"),
+              DropdownSeparator,
+              Page("Blog", "docs-blog", "/docs/blog")
+            ),
+            dropDownRole = Seq.empty
+          ),
+          MenuDropdown(
+            id = "users",
+            name = "Users",
+            href = Some("/users"),
+            items = Seq(
+                Page("Create a User", "create-user", "/create-user"),
+                Page("Create a Service User", "create-service-user", "/create-service-user"),
+                Page("Offboard Users", "offboard-users", "/offboard-users")
+              )
+            )
+        )
+      )
+
+      val expectedJson = """{
+        |  "brand": {
+        |    "name": "MDTP",
+        |    "id": "brand",
+        |    "href": "/",
+        |    "external": false,
+        |    "_type": "TopMenu"
+        |  },
+        |  "topLevelLinks": [
+        |    {
+        |      "name": "Users",
+        |      "id": "users",
+        |      "href": "/users",
+        |      "external": false,
+        |      "_type": "TopMenu"
+        |    },
+        |    {
+        |      "name": "Teams",
+        |      "id": "teams",
+        |      "href": "/teams",
+        |      "external": false,
+        |      "_type": "TopMenu"
+        |    },
+        |    {
+        |      "name": "Repositories",
+        |      "id": "repositories",
+        |      "href": "/repositories",
+        |      "external": false,
+        |      "_type": "TopMenu"
+        |    },
+        |    {
+        |      "name": "Deployments",
+        |      "id": "deployments",
+        |      "external": false,
+        |      "_type": "TopMenu"
+        |    }
+        |  ],
+        |  "dropdowns": [
+        |    {
+        |      "id": "docs",
+        |      "name": "Docs",
+        |      "items": [
+        |        {
+        |          "name": "Docs Home",
+        |          "id": "docs-home",
+        |          "href": "/docs",
+        |          "external": false,
+        |          "_type": "Page"
+        |        },
+        |        {
+        |          "_type": "DropdownSeparator"
+        |        },
+        |        {
+        |          "name": "Blog",
+        |          "id": "docs-blog",
+        |          "href": "/docs/blog",
+        |          "external": false,
+        |          "_type": "Page"
+        |        }
+        |      ],
+        |      "dropDownRole": []
+        |    },
+        |    {
+        |      "id": "users",
+        |      "name": "Users",
+        |      "href": "/users",
+        |      "items": [
+        |       {
+        |          "name": "Create a User",
+        |          "id": "create-user",
+        |          "href": "/create-user",
+        |          "external": false,
+        |                    "_type": "Page"
+        |        },
+        |        {
+        |          "name": "Create a Service User",
+        |          "id": "create-service-user",
+        |          "href": "/create-service-user",
+        |          "external": false,
+        |                    "_type": "Page"
+        |        },
+        |        {
+        |          "name": "Offboard Users",
+        |          "id": "offboard-users",
+        |          "href": "/offboard-users",
+        |          "external": false,
+        |                    "_type": "Page"
+        |        }
+        |      ],
+        |      "dropDownRole": []
+        |    }
+        |  ]
+        |}""".stripMargin
+
+      val actualJson = Json.toJson(menu)
+      actualJson shouldBe Json.parse(expectedJson)
+    }
+  }
